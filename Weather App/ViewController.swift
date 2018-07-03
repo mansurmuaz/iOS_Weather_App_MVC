7625df820cc5351a2426582dddd92a749f05b635
Mansur Muaz  Ekici <mansur.ekici@adesso.com.tr>
2018-07-03 10:19:19 +0300

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Mansur Muaz  Ekici on 3.07.2018.
+//  Copyright © 2018 Adesso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
